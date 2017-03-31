67d419f1953786d149cae6642fc8deace8560dee
sabazusi <kouhei.live@gmail.com>
2017-03-31 18:45:10 +0900

    feat: add ipc message constants

---
+++ dist/constants/ipc-message.js
@@ -0,0 +1,7 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+var CREATE_INITIAL_MEMO = exports.CREATE_INITIAL_MEMO = 'createInitialMemo';
+var EXIT_APP = exports.EXIT_APP = 'exitApp';
---
+++ src/constants/ipc-message.js
@@ -0,0 +1,2 @@
+export const CREATE_INITIAL_MEMO = 'createInitialMemo';
+export const EXIT_APP = 'exitApp';
